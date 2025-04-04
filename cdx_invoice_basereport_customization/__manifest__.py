cea73c2982d8cc6f630fd2c24ce372fe3239d401
Athira Sayanth KV <athirakvsayanth@gmail.com>
2025-04-05 04:47:19 +0530

    Compleated Task

---
+++ cdx_invoice_basereport_customization/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Base Report Custom",
+    "version": "18.0",
+    "depends": ["account"],
+    "data": [
+        "views/view.xml",
+        # "views/report.xml",
+
+
+    ],
+    "installable": True,
+    "application": False,
+}
